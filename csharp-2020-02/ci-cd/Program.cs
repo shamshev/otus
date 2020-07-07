684649e2b80c71c73cbe047f0e1224b6fcc3dc0f
shamshev <sergey.shamshev@nordigy.ru>
2020-07-07 19:18:38 +0300

    c#-2020-02 init

---
+++ csharp-2020-02/ci-cd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace otus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello OTUS students!");
+        }
+    }
+}
